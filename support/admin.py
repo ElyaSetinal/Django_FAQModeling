cf77c65cb0080c41b1883ea867883e32641f66f0
Ra YoungChae <yc.ra.elya@gmail.com>
2022-04-20 21:53:20 +0900

    readonlyfield modify

--- support/admin.py
+++ support/admin.py
@@ -7,8 +7,9 @@ from .models import Faq, Inquiry, Answer
 @admin.register(Faq)
 class Faqmodeladmin(admin.ModelAdmin):
     list_display = ('id', 'category', 'question', 'created_person',
-    'created_time','last_modify_person','last_modified_time')
+    'created_time', 'last_modify_person', 'last_modified_time')
     list_p_page = 20
+    readonly_fields = ('created_time', 'last_modified_time')
 
 class AnswerInline(admin.StackedInline):
     model = Answer
